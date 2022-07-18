f1f700bebe2cdf433267de8490dba91ef8a6dc39
unknown <aminsalkovic2019@gmail.com>
2022-07-18 16:46:53 +0200

    learn how to create dropdown menu-bar

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Card from './components/Card';
-import Counter from './components/Counter';
-import Navbar from './components/Navbar';
-import Todo from './components/Todo'
+import Navbar from './components/Navbar'
 
-
-// const ThemeContext = createContext()
 
 const App =()=> {
   // const[isCardShown,setIsCardShown]=useState(false)
-    
-   const data=["amin","saki","salkovic"]
-
 return (
-    <div className="container">
-      <Todo/>
-      <Card/>
-      
-{data.map}
-
-{/*   
-       <button  onClick={()=>{
-         setIsCardShown(!isCardShown)
-        }}>CLICK ME</button>cd
-      {isCardShown && <Card value={"nesto"}/>} */}
-  
+   
+     <Router>
+       <Navbar/>
+     </Router>
      
-  </div>
+
+
+      //  <button  onClick={()=>{
+      //    setIsCardShown(!isCardShown)
+      //   }}>CLICK ME</button>cd
+      // {isCardShown && <Card value={"nesto"}/>} */}
+  
+  
+   
+ 
   );
 }
 
