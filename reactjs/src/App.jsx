09dbd38d516e2d7e5881431924dbb844ef4c3ac7
unknown <aminsalkovic2019@gmail.com>
2022-07-22 21:01:45 +0200

    fetch users from git hub

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -3,76 +3,168 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Card from './components/Card'
 import Navbar from './components/Navbar'
+import Korisnik from './components/Korisnik'
+import Korisnikp from './components/Korisnikp';
+
 
 //useefect reakcije cele komponente na nesto
 const App =()=> {
+  let formValue = {
+    name: "",
+    email: "",
+    lastName: "",
+  };
+
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    lastName: "",
+  });
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="firstName"
+        onChange={(e) => {
+          formValue.name = e.target.value;
+        }}
+      />
+      <input
+        type="text"
+        placeholder="lastname"
+        onChange={(e) => {
+          formValue.lastName = e.target.value;
+        }}
+      />
+      <input
+        type="text"
+        placeholder="email"
+        onChange={(e) => {
+          formValue.email = e.target.value;
+        }}
+      />
+      <button
+        onClick={() => {
+          if (formValue.name && formValue.lastName && formValue.email) {
+            console.log(formValue);
+            setValue(formValue);
+          } else {
+            console.log("Please fill all the fields");
+          }
+        }}
+      >
+        Submit
+      </button>
+
+      <div>
+        <h1>{value.name}</h1>
+        <h1>{value.lastName}</h1>
+        <h1>{value.email}</h1>
+      </div>
+       
+      <Korisnikp/>
+    </div>
+
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   // const[isCardShown,setIsCardShown]=useState(false)
 
   // const[data,setData]=useState([]);
 
-  const[facts,setFacts]=useState([])
-
-   
-  function getFacts() {
-    fetch("https://catfact.ninja/facts")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setFacts(data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  useEffect(() => {
-    getFacts();
-    console.log("PRVI USEEFFCT");
-  }, []);
-  // let a=""
-return (
-<div className="container">
+//   const[facts,setFacts]=useState([])
  
-<div>
-      {facts.map((el) => {
-        return <h1>{el.fact}</h1>;
-      })}
-    </div>
+//   function getFacts() {
+//     fetch("https://api.github.com/users/dzemildupljak")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data.name);
+//         // setFacts(data.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+
+//   useEffect(() => {
+//     getFacts();
+//   }, []);
+//   // let a=""
+// return (
+// <div className="container">
+ 
+// <div>
+//       {facts.map((el) => {
+//         return <h1>{el.fact}</h1>;
+//       })}
+//     </div>
   
- 
+//  </div>
      
   
 
-     {/* /* <h1>TODO LIST</h1>
+    //   <h1>TODO LIST</h1>
      
-     <div className="unos">
+    //  <div className="unos">
      
 
-      <input type="text" onChange={(e)=>{
-        a=e.target.value
-      }}/>
-      <button className='dugme' onClick={()=>{
-        setData([...data,a])
-      }}>Submit</button>
+    //   <input type="text" onChange={(e)=>{
+    //     a=e.target.value
+    //   }}/>
+    //   <button className='dugme' onClick={()=>{
+    //     setData([...data,a])
+    //   }}>Submit</button>
     
-     </div>
+    //  </div>
      
-     {data.map((el) => {
-       return <Card title={el} />;
-      })} */} 
+    //  {data.map((el) => {
+    //    return <Card title={el} />;
+    //   })} 
     
 
 
 
 
-{/* // return( */}
+
 
 {/* //   <Router>
 //      <Navbar/>
 //    </Router> */}
       
 
-   </div>
+  //  </div>
 
 
 
@@ -108,7 +200,7 @@ return (
     
     
  
-  );
+ 
 }
 
 export default App;
---
+++ reactjs/src/components/Korisnik.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Koirniskp from './Korisnikp';
+import './Korisnik.css'
+const Korisnik=()=>{
+    const [isCardShown,setIsCardShown]=useState(false)
+    return(
+       <div className="karticakorisnika">
+        
+        <input type="text" />
+        <button  onClick={setIsCardShown(!isCardShown)}>ADD</button>
+        {isCardShown && <Koirniskp/>}
+       
+      
+    </div>
+
+    )
+}
+
+export default Korisnik;
