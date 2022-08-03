4451ffe3c8a0bf931040eb3a163e1803af0e6cc9
unknown <aminsalkovic2019@gmail.com>
2022-08-03 20:49:39 +0200

    /

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,26 +1,20 @@
 import React, { useState,useEffect} from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Axios from 'axios'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
-import User from './components/users/User';
-
-
-
 
 
 
 //useefect reakcije cele komponente na nesto
 const App =()=> {
-useEffect(()=>{
-    Aos.init({duration:2000})
-  },[])
+// useEffect(()=>{
+//     Aos.init({duration:2000})
+//   },[])
 
   return ( 
     <div className="container">
-      <div style={{height:"700px"}}></div>
-      <User/>
+      
     </div>
   );
 
