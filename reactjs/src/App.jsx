73cdaa5237ebda1cb8d244672a7af97a427f14c6
unknown <aminsalkovic2019@gmail.com>
2022-08-12 20:20:00 +0200

    /

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,43 +1,56 @@
 import React, { useState,useEffect} from 'react';
 import './App.css';
 import { Route,Routes,Link} from 'react-router-dom';
-import Counter from './components/Counter'
-import Korisnikp from './components/Korisnikp'
-import Drop from './components/Drop'
 import Axios from 'axios'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
-import Lista from './components/Lista';
+import Card from './components/Card';
 
 
 //useefect reakcije cele komponente na nesto
 //use memo upordejuje vrednosti ako su iste nece rirenderovati ako nisu rirenderovace
 
 const App =()=> {
-
+  const [data ,setData]=useState([])
+  let a=""
 
 
 return ( 
   <div className="container">
- 
-       
-      <Routes>
-        <Route path={'counter'} element={<Counter/>}/>
-        <Route path={'korisnikp'} element={<Korisnikp/>}/>
-      </Routes>
-        <div>
-           <Link to={"/counter"}><h1>welcome</h1></Link>
-           <Link to={"/korisnikp"}><h1>click</h1></Link>
-         </div>
-    </div>
+    
+  
+   
+       <input type="text"  onChange={(e)=>{
+            a=e.target.value
+
+       }}/>
+       <button onClick={()=>{
+        setData([...data,a])
+       }}>ADD</button>
+           
+           {data.map((el)=>{
+         return <Card title={el}/>
+           })}
+        </div>
+    
   );
   
   
   
+  {/* <div className="div1">
+ <Routes>
+   <Route path={'counter'} element={<Counter/>}/>
+   <Route path={'korisnikp'} element={<Korisnikp/>}/>
+ </Routes>
+  </div>
+   <div className='div2'>
+      <Link to={"/counter"}><h1>welcome</h1></Link>
+      <Link to={"/korisnikp"}><h1>click</h1></Link>
+    </div> */}
   
   
   // useEffect(()=>{
-  //     Aos.init({duration:2000})
+    //     Aos.init({duration:2000})
   //   },[])
   
   
